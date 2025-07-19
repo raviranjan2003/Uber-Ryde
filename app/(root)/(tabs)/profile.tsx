1d366abd284b3aefcd5075aee126ac3987683491
Ravi <2140131@sliet.ac.in>
2025-07-20 00:37:38 +0530

    root stack

---
+++ app/(root)/(tabs)/profile.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Profile = () => {
+    return (
+        <SafeAreaView>
+            <View>
+                <Text>This is my Profile!</Text>
+            </View>
+        </SafeAreaView>
+    )
+}
+
+export default Profile;
---
+++ app/(root)/(tabs)/rides.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Rides = () => {
+    return (
+        <SafeAreaView>
+            <View>
+                <Text>This is my Rides!</Text>
+            </View>
+        </SafeAreaView>
+    )
+}
+
+export default Rides;
