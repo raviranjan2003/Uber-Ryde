a5530ea763138d8d445748bed1a9f0e584067eff
Ravi <2140131@sliet.ac.in>
2025-07-20 05:21:49 +0530

    feat: UI for SignUp page

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,13 +1,71 @@
-import { Text, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import CustomButton from "@/components/CustomButton";
+import InputField from "@/components/InputField";
+import OAuth from "@/components/OAuth";
+import { icons, images } from "@/constants";
+import { Link } from "expo-router";
+import { useState } from "react";
+import { Image, ScrollView, Text, View } from "react-native";
 
 const SignUp = () => {
+    const [form, setForm] = useState({
+        name: "",
+        email: "",
+        password: ""
+    })
+
+    const handleSignUp = async () => {
+
+    }
+
     return (
-        <SafeAreaView>
-            <View>
-                <Text>This is my SignUp!</Text>
+        <ScrollView className="flex-1 bg-white">
+            <View className="flex-1 bg-white">
+                <View className="relative w-full h-[200px]">
+                    <Image source={images.signUpCar} className="z-0 w-full h-[200px]"/>
+                    <Text className="text-2xl text-black font-JakartaSemiBold absolute bottom-5 left-4">
+                        Create Your Account
+                    </Text>
+                </View>
+                <View className="p-5">
+                    <InputField 
+                        label="Name"
+                        placeholder="Enter Your Name"
+                        icon={icons.person}
+                        value={form.name}
+                        onChange={(value) => setForm({...form, name: value.nativeEvent.text})}
+                    />
+                    <InputField 
+                        label="Email"
+                        placeholder="Enter Your Email"
+                        icon={icons.email}
+                        value={form.email}
+                        onChange={(value) => setForm({...form, email: value.nativeEvent.text})}
+                    />
+                    <InputField 
+                        label="Password"
+                        placeholder="Enter Your Password"
+                        icon={icons.lock}
+                        value={form.password}
+                        secureTextEntry={true}
+                        onChange={(value) => setForm({...form, password: value.nativeEvent.text})}
+                    />
+
+                    <CustomButton
+                        title="Sign Up"
+                        className="mt-6"
+                        onPress={handleSignUp}
+                    />
+
+                    {/* OAuth */}
+                    <OAuth />
+
+                    <Link href="/sign-in" className="text-xs text-center text-general-200 mt-3">
+                        <Text>Already have an account?</Text>
+                        <Text className="text-blue-600">Log In</Text>
+                    </Link>
+                </View>
             </View>
-        </SafeAreaView>
+        </ScrollView>
     )
 }
 
