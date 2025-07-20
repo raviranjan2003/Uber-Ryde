4a5820e9da4e6b0f4b1402d71a52335ec49eab11
Ravi <2140131@sliet.ac.in>
2025-07-21 01:43:27 +0530

    Redirecting to home route
    if any active state found

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,12 @@
+import { useAuth } from "@clerk/clerk-expo";
 import { Redirect } from "expo-router";
 
 const HomePage = () => {
+    const { isSignedIn } = useAuth();
+
+    if(isSignedIn) {
+        return <Redirect href="/(root)/(tabs)/home"/>;
+    }
     return <Redirect href="/(auth)/welcome" />
 }
 
