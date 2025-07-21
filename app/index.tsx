366dedfa02da864bbbf503c77ae2565daf15ea4e
Ravi <2140131@sliet.ac.in>
2025-07-21 15:48:06 +0530

    fix: minor fix loading active state

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,22 @@
 import { useAuth } from "@clerk/clerk-expo";
 import { Redirect } from "expo-router";
+import { ActivityIndicator, View } from "react-native";
 
 const HomePage = () => {
-    const { isSignedIn } = useAuth();
+  const { isSignedIn, isLoaded } = useAuth();
 
-    if(isSignedIn) {
-        return <Redirect href="/(root)/(tabs)/home"/>;
-    }
-    return <Redirect href="/(auth)/welcome" />
-}
+  if (!isLoaded) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
+
+  if (isSignedIn) {
+    return <Redirect href="/(root)/(tabs)/home" />;
+  }
+  return <Redirect href="/(auth)/welcome" />;
+};
 
 export default HomePage;
