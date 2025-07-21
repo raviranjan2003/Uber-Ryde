e15acd6f8715989da57aea9c004ec5a9b2a1ea12
Ravi <2140131@sliet.ac.in>
2025-07-22 01:53:19 +0530

    Component for showing map

---
+++ components/Map.tsx
@@ -0,0 +1,14 @@
+import { Text } from "react-native";
+import MapView, { PROVIDER_DEFAULT } from "react-native-maps";
+
+const Map = () => (
+    <MapView 
+        provider={PROVIDER_DEFAULT} 
+        className="w-full h-full"
+        tintColor="black"
+    >
+        <Text>Map</Text>
+    </MapView>
+)
+
+export default Map;
