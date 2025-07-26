0c9f7cf0732a3c5d7a7a59b84112479e9d9117f8
Ravi <2140131@sliet.ac.in>
2025-07-26 23:28:32 +0530

    signup/signin logic
    using clerk

--- components/OAuth.tsx
+++ components/OAuth.tsx
@@ -1,11 +1,57 @@
 import { icons } from "@/constants";
+import { fetchAPI } from "@/lib/fetch";
+import { useSSO } from "@clerk/clerk-expo";
+import * as AuthSession from 'expo-auth-session';
+import { useCallback } from "react";
 import { Image, Text, View } from "react-native";
 import CustomButton from "./CustomButton";
 
 const OAuth = () => {
-    const handleGoogleSignUp = async () => {
+    const { startSSOFlow } = useSSO()
+    const handleGoogleSignUp = useCallback(async () => {
+        console.log("Google Signup");
+    try {
+      // Start the authentication process by calling `startSSOFlow()`
+      const { createdSessionId, setActive, signIn, signUp } = await startSSOFlow({
+        strategy: 'oauth_google',
+        // For web, defaults to current path
+        // For native, you must pass a scheme, like AuthSession.makeRedirectUri({ scheme, path })
+        // For more info, see https://docs.expo.dev/versions/latest/sdk/auth-session/#authsessionmakeredirecturioptions
+        redirectUrl: AuthSession.makeRedirectUri({
+            scheme: "ubercrn",
+            path: "/(root)/(tabs)/home"
+        }),
+      })
 
+      // If sign in was successful, set the active session
+      if (createdSessionId) {
+        if(setActive) {
+            await setActive!({ session: createdSessionId });
+
+            if(signUp && signUp.status === 'complete') {
+                // console.log("SignUp data==>",signUp);
+                if(signUp?.createdUserId) {
+                    await fetchAPI("/(api)/user", {
+                        method: "POST",
+                        body: JSON.stringify({
+                            name: `${signUp.firstName} ${signUp.lastName}`,
+                            email: signUp.emailAddress,
+                            clerkId: signUp.createdUserId
+                        })
+                    })
+                }
+            }else if(signIn) {
+                // console.log("SignIn data ==>", signIn);
+                // Alert.alert("Signedin Successfully, redirecting to homepage...");
+            }
+        }
+      } 
+    } catch (err) {
+      // See https://clerk.com/docs/custom-flows/error-handling
+      // for more info on error handling
+      console.error(JSON.stringify(err, null, 2))
     }
+  }, [])
 
     return (
         <View>
